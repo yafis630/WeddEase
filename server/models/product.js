6ec96144ad08a4b95bf1875cc33181b5a41ad47c
jasia hassan <jasiahassan120@gmail.com>
2023-08-14 22:26:10 +0530

    updates

--- server/models/product.js
+++ server/models/product.js
@@ -23,6 +23,11 @@ const productSchema = new mongoose.Schema({
     required: true,
     trim:true
   },
+  category:{
+    type: String,
+    required: true,
+    trim:true
+  }
 });
 
 module.exports = mongoose.model('Product', productSchema);
