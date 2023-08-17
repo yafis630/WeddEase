028e1ed99c20cef233b10606f778cdbe6519f1ba
mouminamajeed <81755364+mouminamajeed@users.noreply.github.com>
2023-08-17 16:17:23 +0530

    update

--- client/src/components/SellerButtons.js
+++ client/src/components/SellerButtons.js
@@ -1,72 +1,39 @@
-/*import React from "react";
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
 import Header from './Header';
-import Footer from './Footer';
-import sellerCategories from "../data/sellerCategories";
-import SellerHolder from "./SellerHolder";
-import "../styles/ShoppingServices.css";
+import '../styles/SellerButtons.css';
 
-const ShoppingServices = () => {
-  const categoryItems = sellerCategories.map((category) => (
-    <li className="category-card" key={category.id}>
-      <a href={`/sellers/${category.name}`}>
-        <img src={category.image} alt={category.name} />
-      </a>
-      <div className="category-card-content">
-        <h3>{category.name}</h3>
-      </div>
-    </li>
-  ));
-
+const SellerButtons = () => {
   return (
     <div>
-      <div className="back-img">
-        <Header />
-        <div className="shopping-services-container">
-          <h1>Explore Shopping Services</h1>
-          <ul className="category-list">{categoryItems}</ul>
+      <div className="home-page">
+      <Header />
+        <div className="content-container">
+          <h2 className="welcome-text">Welcome Seller/Worker</h2>
+          <h5>Please Login to your account to continue. If you don't have
+            an account, please register.
+          </h5>
+
+          <div className="button-container">
+            <Link to="/WorkerRegistration">
+              <Button variant="primary" className="worker-button">Register as Worker</Button>
+            </Link>
+            <Link to="/WorkerLogin">
+              <Button variant="primary" className="worker-button">Login as Worker</Button>
+            </Link>
+            <Link to = "/SellerRegistration">
+            <Button variant="secondary" className="seller-button">Register as Seller</Button>
+            </Link>
+            <Link to="/SellerLogin">
+            <Button variant="secondary" className="seller-button">Login as Seller</Button>
+            </Link>
+          </div>
         </div>
-        <Footer />
       </div>
     </div>
   );
 };
 
-export default ShoppingServices;*/
-import React from "react";
-import Header from "./Header";
-import Footer from "./Footer";
-import sellerCategories from "../data/sellerCategories";
+export default SellerButtons;
 
-
-import "../styles/ShoppingServices.css";
-
-const ShoppingServices = () => {
-  const categoryItems = sellerCategories.map((category) => (
-    
-   <li className="category-card" key={category.id}>
-      <a href={`/sellers/${category.name}`}>
-        <img src={category.image} alt={category.name} />
-      </a>
-      <div className="category-card-content">
-        <h3>{category.name}</h3>
-      </div>
-  
-    </li>
-  ));
-
-  return (
-    <div>
-      <div className="back-img">
-        <Header />
-        <div className="shopping-services-container">
-          <h1>Explore Shopping Services</h1>
-          <ul className="category-list">{categoryItems}</ul>
-         {/* <Link to="/productscard">View Products</Link> Link to ProductsCard */}
-        </div>
-        <Footer />
-      </div>
-    </div>
-  );
-};
-
-export default ShoppingServices;
