a20b9317c18d747b2891f6d051ee589913b3e1e4
Mohd Yafis Khan <yafis.khan@rediffmail.com>
2023-08-18 13:09:29 +0530

    Updates

--- client/src/components/WorkerHome.js
+++ client/src/components/WorkerHome.js
@@ -7,7 +7,6 @@ import { isSameDay } from "date-fns"; // Import addDays function
 import "../styles/WorkerHome.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import profile from "../data/profile-placeholder.png";
 import AuthContext from "../context/AuthProvider";
 import Logout from "./Logout";
 
@@ -51,7 +50,9 @@ const { category , workerId} = useParams();
                 <p>{worker.gender}</p>
                 <h3>Bio</h3>
                 <p>{worker.bio}</p>
-              
+                <Button className="update-btn" variant="info" href="/UpdateProfile">
+        Update Profile
+      </Button>
             </div>
 
           ));
@@ -107,9 +108,7 @@ const { category , workerId} = useParams();
       
       <div className="worker-display">{workerList}</div>
       <Logout />
-      <Button className="update-btn" variant="info" href="/UpdateProfile">
-        Update Profile
-      </Button>
+      
       <div className="calendar-container">
         <h4>Unavailable Dates</h4>
         <Calendar
